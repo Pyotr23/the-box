6d27b1a35e7d7d5f344600b090e334c0b7b2855d
Pyotr Kolesnikov <2chilavert3@mail.ru>
2023-03-20 00:03:42 +0300

    Update project template

--- arduino/the-box/the-box.ino
+++ arduino/the-box/the-box.ino
@@ -1,16 +1,32 @@
+#include "DHT.h"
+#define DHTPIN 2
+
+DHT dht(DHTPIN, DHT11);
+
 const int LED = 13;
 
-int val;
+int val; 
+float t;   
+char buffer[5];    
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
+  dht.begin();
 }
 
 void loop() {
   if (Serial.available()) {
-     val = Serial.read();
+    val = Serial.read();
+    if (val == '2') {
+      t = dht.readTemperature();
+      if (isnan(t)) {
+        Serial.write("read temperature error");
+      } else {
+        Serial.write(dtostrf(t, 4, 1, buffer));
+      }
+     }
      if (val == '1') {
       digitalWrite(LED, HIGH);
       Serial.write("diode one");
